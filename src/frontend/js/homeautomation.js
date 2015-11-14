30bc0ddb6380968e99e2c66be1ca31de17c22078
dynek <pierrick@bs-network.net>
2015-11-14 20:58:20 +0100

    handling json sent by server

--- src/frontend/js/homeautomation.js
+++ src/frontend/js/homeautomation.js
@@ -12,10 +12,30 @@ var __HOMEAUTOMATION_JS = null;
 var homeautomation = function() {
 
   // process JSON
-  var processJSON = function(data) {
+  var processJSON = function(json) {
     common.logMessage("processing JSON data");
 
-    $("#container").append("<pre>" + JSON.stringify(data, null, 2) + "</pre>");
+    // ensure action and data have been specified
+    if(typeof json.action === 'undefined') { throw new Error("action is missing in received json"); }
+    if(json.action != "reload" && typeof json.data === 'undefined') { throw new Error("data missing in received json"); }
+
+    // tasks based on action
+    switch(json.action) {
+      // server requested that page be reloaded
+      case "reload":
+        common.reloadPage();
+        break;
+
+      // as an example, printing data
+      case "print":
+        $("#container").append("<pre>" + JSON.stringify(json.data, null, 2) + "</pre>");
+        break;
+
+      default:
+        throw new Error("unknown action '" + json.action + "' specified in received json");
+    }
+
+
   };
 
   // expose functions
